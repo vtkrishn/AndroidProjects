7cbe75938b9211248ab8cb6736bf5062af52e76c
Vinod Krishnan <vin.sinin@gmail.com>
2017-06-03 20:33:03 -0700

    'changes'

--- DSAlgorithmVisualization/app/src/main/java/dsalgorithm/vtkrishn/com/dsalgorithmvisualization/MainActivity.java
+++ DSAlgorithmVisualization/app/src/main/java/dsalgorithm/vtkrishn/com/dsalgorithmvisualization/MainActivity.java
@@ -1,7 +1,11 @@
 package dsalgorithm.vtkrishn.com.dsalgorithmvisualization;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        customizeTitleBar();
+    }
+
+    private void customizeTitleBar(){
+        Window window = this.getWindow();
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.setStatusBarColor(ContextCompat.getColor(this,R.color.colorAccent));
+        getSupportActionBar().setBackgroundDrawable(
+                new ColorDrawable(ContextCompat.getColor(this,R.color.colorAccent)));
     }
 }
